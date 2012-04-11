07d6bdc0491971242d18e95fcaba38282b92cb34
hibariya <celluloid.key@gmail.com>
2012-04-11 23:06:55 +0900

    add version command

--- lib/reditor.rb
+++ lib/reditor.rb
@@ -1,5 +1,44 @@
-require 'reditor/version'
+require 'pathname'
+require 'rubygems'
 
 module Reditor
+  class LibraryNotFound < LoadError; end
+
+  extend self
+
+  def detect(name)
+    path = detect_library_path(name) or raise LibraryNotFound, 'No library found'
+
+    if path.file?
+      [path.dirname.to_path, path.basename.to_path]
+    else
+      [path.to_path, '.']
+    end
+  end
+
+  def detect_library_path(name)
+    detect_library_path_from_loadpath(name) || detect_library_path_from_gem(name)
+  end
+
+  def detect_library_path_from_loadpath(name)
+    basename = "#{name}.rb"
+
+    $LOAD_PATH.map {|path|
+      full_path = File.expand_path(path + '/' + basename)
+
+      Pathname.new(full_path)
+    }.detect(&:exist?)
+  end
+
+  # TODO: care version specification
+  def detect_library_path_from_gem(name)
+    spec = Gem::Specification.find_by_name(name.to_s)
+
+    Pathname.new(spec.full_gem_path)
+  rescue Gem::LoadError
+    nil
+  end
+
   autoload :Command, 'reditor/command'
+  autoload :VERSION, 'reditor/version'
 end
--- lib/reditor/command.rb
+++ lib/reditor/command.rb
@@ -1,13 +1,11 @@
 # coding: utf-8
 
 require 'thor'
-require 'pathname'
-require 'rubygems'
 
 module Reditor
-  class LibraryNotFound < LoadError; end
-
   class Command < Thor
+    include Reditor
+
     desc :open, 'open a library'
     def open(name)
       dir, file = *detect(name)
@@ -22,6 +20,11 @@ module Reditor
       say e.message, :red
     end
 
+    desc :version, 'show version'
+    def version
+      say "Reditor version #{VERSION}"
+    end
+
     # XXX FIXME: a more better implementation
     def method_missing(name, *args, &block)
       send :open, name
@@ -31,39 +34,6 @@ module Reditor
 
     def editor_command
       ENV['EDITOR']
-    end
-
-    def detect(name)
-      path = detect_library_path(name) or raise LibraryNotFound, 'No library found'
-
-      if path.file?
-        [path.dirname.to_path, path.basename.to_path]
-      else
-        [path.to_path, '.']
-      end
-    end
-
-    def detect_library_path(name)
-      detect_library_path_from_loadpath(name) || detect_library_path_from_gem(name)
-    end
-
-    def detect_library_path_from_loadpath(name)
-      basename = "#{name}.rb"
-
-      $LOAD_PATH.map {|path|
-        full_path = File.expand_path(path + '/' + basename)
-
-        Pathname.new(full_path)
-      }.detect(&:exist?)
-    end
-
-    # TODO: care version specification
-    def detect_library_path_from_gem(name)
-      spec = Gem::Specification.find_by_name(name.to_s)
-
-      Pathname.new(spec.full_gem_path)
-    rescue Gem::LoadError
-      nil
     end
   end
 end
